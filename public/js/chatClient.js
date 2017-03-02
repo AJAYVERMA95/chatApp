911077637df658a21ab5c81cad0ea6582c22e325
Ajay Verma <ajay_verma@ajay.av-Compaq-Presario-C700-Notebook-PC>
2017-03-02 17:53:47 +0530

    going to particular chat after clicking online users list.

--- public/js/chatClient.js
+++ public/js/chatClient.js
@@ -1,12 +1,5 @@
 var socket = io();
 
-var getAllOnlineUsers = function(){
-  socket.on('allOnlineUsers',function (list) {
-    delete list[this.id];
-    showAllOnlineUsersInChat(list);
-  });
-}
-
 $('#login-modal .btn.save').click(function(){
   var newUser = $('input').val();
   console.log(newUser);
@@ -14,6 +7,14 @@ $('#login-modal .btn.save').click(function(){
   $('#login-page').toggle();
   $('#hangout').toggle();
   $('#hangout #head.style-bg h1').html(newUser);
-  getAllOnlineUsers();
+});
 
+socket.on('allOnlineUsers',function (list) {
+  delete list[this.id];
+  // showAllOnlineUsersInChat
+  var $location = $('.list-text > ul');
+  $location.empty();
+  $.each(list,function(userId,userName){
+    $location.append('<li><img src="http://lorempixel.com/100/100/people/1/"><div class="content-container"><span class="name">'+userName +'</span><span class="txt">Online</span></div><span class="time">'+(new Date)+'</span></li>');
+  });
 });
