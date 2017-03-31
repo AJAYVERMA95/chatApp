959b0560f54739a1ad8775aa8390c5d441e636c4
Ajay Verma <ajay_verma@ajay.av-Compaq-Presario-C700-Notebook-PC>
2017-03-31 20:46:57 +0530

    port set to run on heroku.

--- server.js
+++ server.js
@@ -2,9 +2,8 @@ var app = require("./app");
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
 
-var port = 3000 || process.env.PORT;
-server.listen(port,function(){
-    console.log("server running at "+port);
+server.listen(app.get('port'),function(){
+    console.log("server running at "+ app.get('port'));
 });
 var createUser = function (nickName,userSocket) {
   userSocket.nickName = nickName;
